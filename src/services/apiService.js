9d8ef5a53d64b9b54fa17bd7c9f1e08fe5781ac9
Gabriel Ferreira <gabrielferreira.dev@hotmail.com>
2023-11-13 20:08:33 -0300

    prod: last commit

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { clearCredentials } from '../redux/authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:8000',
+  baseUrl: 'https://meconomic-api.onrender.com',
   // credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
